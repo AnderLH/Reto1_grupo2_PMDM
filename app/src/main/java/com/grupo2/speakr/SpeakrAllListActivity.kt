2aaecbabfcbdc9d26220ab200e8f194020dc2395
in2dm3-v <in2dm3-v@elorrieta.int>
2023-10-18 19:25:23 +0200

    Vistas Login y Register hechas

---
+++ app/src/main/java/com/grupo2/speakr/SpeakrAllListActivity.kt
@@ -0,0 +1,11 @@
+package com.grupo2.speakr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpeakrAllListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speakr_all_list)
+    }
+}
