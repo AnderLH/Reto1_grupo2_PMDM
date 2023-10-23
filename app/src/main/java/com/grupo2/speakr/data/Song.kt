515f94e023f7a3dfffa824a204b02fc74cbe5a78
AnderLH <ander.lopezdevallejohi@elorrieta-errekamari.com>
2023-10-23 19:28:35 +0200

    data models created

--- app/src/main/java/com/grupo2/speakr/data/Song.kt
+++ app/src/main/java/com/grupo2/speakr/data/Song.kt
@@ -1,4 +1,12 @@
 package com.grupo2.speakr.data
 
-class Song {
-}
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class Song (
+    val id: Int,
+    val title: String,
+    val author: String,
+    val url: String,
+): Parcelable
--- app/src/main/java/com/grupo2/speakr/data/User.kt
+++ app/src/main/java/com/grupo2/speakr/data/User.kt
@@ -1,4 +1,13 @@
 package com.grupo2.speakr.data
 
-class User {
-}
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User (
+    val id: Int,
+    val name: String,
+    val surname: String,
+    val email: String,
+    val password: String,
+): Parcelable
