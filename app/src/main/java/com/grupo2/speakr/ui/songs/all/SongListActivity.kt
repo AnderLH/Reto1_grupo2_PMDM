43e8461fee93d8bc1fbb8d422f141f0d27465571
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-10-24 15:29:13 +0200

    song list + view

--- app/src/main/java/com/grupo2/speakr/ui/songs/all/SongListActivity.kt
+++ app/src/main/java/com/grupo2/speakr/ui/songs/all/SongListActivity.kt
@@ -1,12 +1,50 @@
 package com.grupo2.speakr.ui.songs.all
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.grupo2.speakr.R
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.Observer
+import androidx.activity.viewModels
 
-class SongListActivity : AppCompatActivity() {
+import com.grupo2.speakr.data.repository.remote.RemoteSongDataSource
+import com.grupo2.speakr.databinding.ActivitySongsListBinding
+import com.grupo2.speakr.utils.Resource
+
+class SongListActivity : ComponentActivity() {
+
+    private lateinit var songListAdapter: SongListAdapter
+
+    private val songRepository = RemoteSongDataSource()
+
+    private val viewModel: SongViewModel by viewModels { SongsViewModelFactory(songRepository) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_speakr_all_list)
+
+        val binding = ActivitySongsListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        viewModel.items.observe(this, Observer {
+            // esto es lo que se ejecuta cada vez que la lista en el VM cambia de valor
+            Log.e("PruebasDia1", "ha ocurrido un cambio en la lista")
+
+            when (it.status) {
+                Resource.Status.SUCCESS -> {
+                    if (!it.data.isNullOrEmpty()) {
+                        songListAdapter.submitList(it.data)
+                    }
+                }
+                Resource.Status.ERROR -> {
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                }
+                Resource.Status.LOADING -> {
+                    // de momento
+                }
+            }
+
+            //
+        })
     }
+
 }
--- app/src/main/java/com/grupo2/speakr/ui/songs/all/SongListAdapter.kt
+++ app/src/main/java/com/grupo2/speakr/ui/songs/all/SongListAdapter.kt
@@ -1,4 +1,54 @@
 package com.grupo2.speakr.ui.songs.all
 
-class SongListAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.grupo2.speakr.data.Song
+import com.grupo2.speakr.databinding.ItemSongsBinding
+
+class SongListAdapter (
+    private val onClickListener: (Song) -> Unit
+): ListAdapter<Song, SongListAdapter.DepartmentViewHolder>(DepartmentDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DepartmentViewHolder {
+        val binding =
+            ItemSongsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DepartmentViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DepartmentViewHolder, position: Int) {
+        val department = getItem(position)
+        holder.bind(department)
+        holder.itemView.setOnClickListener(){
+            onClickListener(department)
+            Log.e("statusInfo", "departmentAdapter " + department.id.toString())
+
+        }
+    }
+
+    inner class DepartmentViewHolder(private val binding: ItemSongsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(song: Song) {
+            binding.textViewTitle.text = song.title
+            binding.textViewSubtitle1.text = song.author
+            binding.textViewSubtitle2.text = song.url
+        }
+    }
+
+    class DepartmentDiffCallback : DiffUtil.ItemCallback<Song>() {
+
+        override fun areItemsTheSame(oldItem: Song, newItem: Song): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Song, newItem: Song): Boolean {
+            return (oldItem.id == newItem.id && oldItem.title == newItem.title && oldItem.author == newItem.author && oldItem.url == newItem.url)
+            // return oldItem == newItem
+        }
+
+    }
 }
