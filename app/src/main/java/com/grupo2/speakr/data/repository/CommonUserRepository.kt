47d45f630372bd83362b2a173e22cbd8ce45f933
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2023-11-15 19:22:51 +0100

    change Email done

--- app/src/main/java/com/grupo2/speakr/data/repository/CommonUserRepository.kt
+++ app/src/main/java/com/grupo2/speakr/data/repository/CommonUserRepository.kt
@@ -1,7 +1,7 @@
 package com.grupo2.speakr.data.repository
 
 import com.grupo2.speakr.data.LoginUser
-import com.grupo2.speakr.data.MailChange
+import com.grupo2.speakr.data.MailAuth
 import com.grupo2.speakr.data.PasswordAuth
 import com.grupo2.speakr.data.User
 import com.grupo2.speakr.utils.Resource
@@ -13,5 +13,5 @@ interface CommonUserRepository {
 
     suspend fun changePassword(passwordAuth : PasswordAuth) : Resource<Void>
 
-    suspend fun changeEmail(mailChange : MailChange) : Resource<Int>
+    suspend fun changeEmail(mailAuth : MailAuth) : Resource<Void>
 }
--- app/src/main/java/com/grupo2/speakr/ui/users/email/EmailFragment.kt
+++ app/src/main/java/com/grupo2/speakr/ui/users/email/EmailFragment.kt
@@ -1,11 +1,23 @@
 package com.grupo2.speakr.ui.users.email
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.grupo2.speakr.R
+import com.grupo2.speakr.data.MailAuth
+import com.grupo2.speakr.data.PasswordAuth
+import com.grupo2.speakr.data.repository.remote.RemoteUserDataSource
+import com.grupo2.speakr.ui.users.password.PasswordViewModel
+import com.grupo2.speakr.ui.users.password.PasswordViewModelFactory
+import com.grupo2.speakr.utils.Resource
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +31,8 @@ private const val ARG_PARAM2 = "param2"
  */
 class EmailFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    private val userRepository = RemoteUserDataSource()
+    private val viewModel: EmailViewModel by viewModels { EmailViewModelFactory(userRepository) }
     private var param1: String? = null
     private var param2: String? = null
 
@@ -35,7 +49,79 @@ class EmailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_email, container, false)
+        val view = inflater.inflate(R.layout.fragment_email, container, false)
+
+        // Assuming you have EditText views with IDs "editText1" and "editText2"
+        val editText1 = view.findViewById<EditText>(R.id.editTextCurrentEmail)
+        val editText2 = view.findViewById<EditText>(R.id.editTextNewEmail)
+        val editText3 = view.findViewById<EditText>(R.id.editTextConfirmEmail)
+        val buttonView = view.findViewById<Button>(R.id.btnChangeEmail)
+
+        buttonView.setOnClickListener {
+            // Collect data from the EditText views
+            val oldEmail = editText1.text.toString()
+            val newEmail = editText2.text.toString()
+            val newEmailComprob = editText3.text.toString()
+
+
+            if (newEmail == newEmailComprob) {
+                val emailAuth = MailAuth(oldEmail, newEmail)
+
+                viewModel.updateMail(emailAuth)
+                Log.i("password", emailAuth.oldMail )
+                Log.i("password", emailAuth.newMail )
+
+                viewModel.emails.observe(this) { result ->
+                    Log.i("result", result?.status.toString())
+                    Log.i("result", "Received data: ${result?.data}")
+                    if (result != null) {
+                        when (result.status) {
+                            Resource.Status.SUCCESS -> {
+                                // Handle successful password change
+//                                if (result != null) {
+                                Log.i("cambio", "success")
+                                editText1.setText("")
+                                editText2.setText("")
+                                editText3.setText("")
+                                Toast.makeText(requireContext(), "Email changed successfully!", Toast.LENGTH_SHORT).show()
+
+//                                closeFragment()
+
+
+//                                    }
+                            }
+
+                            Resource.Status.ERROR -> {
+                                // Handle password change error
+                                // Show an error toast message
+                                Toast.makeText(requireContext(), "Error changing Email. Please try again.", Toast.LENGTH_SHORT).show()
+                            }
+
+                            Resource.Status.LOADING -> {
+                                // Handle loading state (optional)
+                                // You can show a loading indicator or perform other actions while waiting
+                            }
+                        }
+                    }
+                }
+            } else {
+                // Passwords do not match
+                // Show a toast message indicating the mismatch
+
+                editText3.setText("")
+                Toast.makeText(
+                    requireContext(),
+                    "New mails do not match. Please check again.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            // Do something with the collected data
+            // For example, you can log it or pass it to your ViewModel
+
+            // viewModel.updateData(text1, text2)
+        }
+
+        return view
     }
 
     companion object {
